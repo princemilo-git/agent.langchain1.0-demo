709eab1a03370326be7c83d9679736022413b9cb
DMS-TCCN-Dev-liuxing <liu.xing@toyotaconnected.com.cn>
2026-01-05 14:56:54 +0800

    add SQL agent HITL

--- tutorials/RAG-agent/12-RAG-agent.py
+++ tutorials/RAG-agent/12-RAG-agent.py
@@ -1,8 +1,6 @@
 
 ## (langchain1.0-py311) D:\Work\Workspace\AIProjects\Agent\langchain1.0-demo>python .\tutorials\RAG-agent\12-RAG-agent.py
 
-from langchain_core.tools import tool
-
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 import os
@@ -22,14 +20,42 @@ vector_store = Chroma(
     persist_directory="./chroma_rag_db"
 )
 
+# 2. Retrieval and Generation
+
+# RAG applications commonly work as follows:
+# Retrieve: Given a user input, relevant splits are retrieved from storage using a Retriever.
+# Generate: A model produces an answer using a prompt that includes both the question with the retrieved data
+
+# 检索与生成
+#
+# RAG（检索增强生成）应用通常按以下方式工作：
+#
+# 检索（Retrieve）：给定用户输入，使用检索器（Retriever）从存储中获取相关的文本片段（splits）。
+# 生成（Generate）：模型基于一个包含用户问题和检索到的数据的提示（prompt），生成最终的回答。
+
+# 现在，我们来编写实际的应用逻辑。我们希望创建一个简单的应用程序：接收用户的问题，搜索与该问题相关的文档，将检索到的文档和原始问题一起传递给模型，并返回答案。
+#
+# 我们将演示以下两种方法：
+#
+# 一个使用简单工具执行搜索的 RAG 智能体（RAG agent），这是一种通用性较强的标准实现方式。
+# 一个 两步 RAG 链（two-step RAG chain），每次查询仅调用一次大语言模型（LLM），这种方法对于简单查询而言快速且高效。
+
+# RAG agents
+
+# One formulation of a RAG application is as a simple agent with a tool that retrieves information. We can assemble a minimal RAG agent by implementing a tool that wraps our vector store:
+# RAG 应用的一种实现方式是将其构建为一个带有信息检索工具的简单智能体（agent）。我们可以通过实现一个封装了向量数据库的工具，来组装一个极简的 RAG 智能体。
+
+from langchain.tools import tool
+
 @tool(response_format="content_and_artifact")
-def retrive_context(query: str) -> str:
-    """Retrieve information to help answer a query"""
-    retriveded_docs = vector_store.similarity_search(query, k=2)
-    content = '\n\n'.join(
-        (f"Source:{doc.metadata}\nContent:{doc.page_content}") for doc in retriveded_docs
+def retrieve_context(query: str):
+    """Retrieve information to help answer a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\nContent: {doc.page_content}")
+        for doc in retrieved_docs
     )
-    return content, retriveded_docs
+    return serialized, retrieved_docs
 
 model = init_chat_model(
     model_provider="openai",
@@ -39,25 +65,44 @@ model = init_chat_model(
     temperature=0.7
 )
 
-tools=[retrive_context, ]
+tools=[retrieve_context]
 
-SYSTEM_PROMPT = """
-    你可以使用信息检索工具，回答用户的问题
-"""
+# SYSTEM_PROMPT = """
+#     你可以使用信息检索工具，回答用户的问题
+# """
 
 # 创建Agent
+# from langchain.agents import create_agent
+# agent = create_agent(
+#     model=model,
+#     tools=tools,
+#     system_prompt=SYSTEM_PROMPT,
+# )
 from langchain.agents import create_agent
-agent = create_agent(
-    model=model,
-    tools=tools,
-    system_prompt=SYSTEM_PROMPT,
+
+# If desired, specify custom instructions
+prompt = (
+    "You have access to a tool that retrieves context from a blog post. "
+    "Use the tool to help answer user queries."
+)
+agent = create_agent(model, tools, system_prompt=prompt)
+
+# results = agent.invoke(
+#     {"messages":[{"role":"user", "content":"讲一下Meta官宣收购智能体初创公司Manus"}]}
+# )
+#
+# messages = results['messages']
+# print(f"历史消息： {len(messages)} 条")
+# for message in messages:
+#     message.pretty_print()
+
+query = (
+    "What is the standard method for Task Decomposition?\n\n"
+    "Once you get the answer, look up common extensions of that method."
 )
 
-results = agent.invoke(
-    {"messages":[{"role":"user", "content":"讲一下Meta官宣收购智能体初创公司Manus"}]}
-)
-
-messages = results['messages']
-print(f"历史消息： {len(messages)} 条")
-for message in messages:
-    message.pretty_print()
+for event in agent.stream(
+    {"messages": [{"role": "user", "content": query}]},
+    stream_mode="values",
+):
+    event["messages"][-1].pretty_print()
---
+++ tutorials/SQL-agent/01-sql-agent.py
@@ -0,0 +1,160 @@
+
+## (langchain1.0-py311) D:\Work\Workspace\AIProjects\Agent\langchain1.0-demo>python .\tutorials\SQL-agent\01-sql-agent.py
+
+# 1. Select an LLM
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+import os
+
+load_dotenv("./env/.env")
+
+model = init_chat_model(
+    model_provider="openai",
+    model="qwen-plus",
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    temperature=0.7
+)
+
+# 2. Configure the database
+import requests, pathlib
+
+url = "https://storage.googleapis.com/benchmarks-artifacts/chinook/Chinook.db"
+# local_path = pathlib.Path("Chinook.db")
+
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+local_path = SCRIPT_DIR / "Chinook.db"
+
+# print("当前工作目录（os.getcwd()）:", os.getcwd())
+# print("Path('Chinook.db') 的绝对路径:", local_path.resolve())
+
+if local_path.exists():
+    print(f"{local_path} already exists, skipping download.")
+else:
+    response = requests.get(url)
+    if response.status_code == 200:
+        local_path.write_bytes(response.content)
+        print(f"File downloaded and saved as {local_path}")
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+
+from langchain_community.utilities import SQLDatabase
+
+# db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+db = SQLDatabase.from_uri(f"sqlite:///{local_path.resolve().as_posix()}")
+
+# print(f"Dialect: {db.dialect}")
+# print(f"Available tables: {db.get_usable_table_names()}")
+# print(f'Sample output: {db.run("SELECT * FROM Artist LIMIT 5;")}')
+
+# Dialect: sqlite
+# Available tables: ['Album', 'Artist', 'Customer', 'Employee', 'Genre', 'Invoice', 'InvoiceLine', 'MediaType', 'Playlist', 'PlaylistTrack', 'Track']
+# Sample output: [(1, 'AC/DC'), (2, 'Accept'), (3, 'Aerosmith'), (4, 'Alanis Morissette'), (5, 'Alice In Chains')]
+
+# 3. Add tools for database interactions
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+
+toolkit = SQLDatabaseToolkit(db=db, llm=model)
+
+tools = toolkit.get_tools()
+
+# for tool in tools:
+#     print(f"{tool.name}: {tool.description}\n")
+
+# sql_db_query: Input to this tool is a detailed and correct SQL query, output is a result from the database. If the query is not correct, an error message will be returned. If an error is returned, rewrite the query, check the query, and try again. If you encounter an issue with Unknown column 'xxxx' in 'field list', use sql_db_schema to query the correct table fields.
+#
+# sql_db_schema: Input to this tool is a comma-separated list of tables, output is the schema and sample rows for those tables. Be sure that the tables actually exist by calling sql_db_list_tables first! Example Input: table1, table2, table3
+#
+# sql_db_list_tables: Input is an empty string, output is a comma-separated list of tables in the database.
+#
+# sql_db_query_checker: Use this tool to double check if your query is correct before executing it. Always use this tool before executing a query with sql_db_query!
+
+# 4. Use create_agent
+system_prompt = """
+You are an agent designed to interact with a SQL database.
+Given an input question, create a syntactically correct {dialect} query to run,
+then look at the results of the query and return the answer. Unless the user
+specifies a specific number of examples they wish to obtain, always limit your
+query to at most {top_k} results.
+
+You can order the results by a relevant column to return the most interesting
+examples in the database. Never query for all the columns from a specific table,
+only ask for the relevant columns given the question.
+
+You MUST double check your query before executing it. If you get an error while
+executing a query, rewrite the query and try again.
+
+DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the
+database.
+
+To start you should ALWAYS look at the tables in the database to see what you
+can query. Do NOT skip this step.
+
+Then you should query the schema of the most relevant tables.
+""".format(
+    dialect=db.dialect,
+    top_k=5,
+)
+
+from langchain.agents import create_agent
+from langchain.agents.middleware import HumanInTheLoopMiddleware
+from langgraph.checkpoint.memory import InMemorySaver
+
+
+agent = create_agent(
+    model,
+    tools,
+    system_prompt=system_prompt,
+    middleware=[
+        HumanInTheLoopMiddleware(
+            interrupt_on={"sql_db_query": True},
+            description_prefix="Tool execution pending approval",
+        ),
+    ],
+    checkpointer=InMemorySaver(),
+)
+
+# 5. Run the agent
+question = "Which genre on average has the longest tracks?"
+config = {"configurable": {"thread_id": "1"}}
+
+for step in agent.stream(
+    {"messages": [{"role": "user", "content": question}]},
+    config,
+    stream_mode="values",
+):
+    if "__interrupt__" in step:
+        print("INTERRUPTED:")
+        interrupt = step["__interrupt__"][0]
+        for request in interrupt.value["action_requests"]:
+            print(request["description"])
+    if "messages" in step:
+        step["messages"][-1].pretty_print()
+    else:
+        pass
+
+# ...
+#
+# INTERRUPTED:
+# Tool execution pending approval
+#
+# Tool: sql_db_query
+# Args: {'query': 'SELECT g.Name AS Genre, AVG(t.Milliseconds) AS AvgTrackLength FROM Track t JOIN Genre g ON t.GenreId = g.GenreId GROUP BY g.Name ORDER BY AvgTrackLength DESC LIMIT 1;'}
+
+# 6. Implement human-in-the-loop review
+from langgraph.types import Command
+
+for step in agent.stream(
+    Command(resume={"decisions": [{"type": "approve"}]}),
+    config,
+    stream_mode="values",
+):
+    if "messages" in step:
+        step["messages"][-1].pretty_print()
+    if "__interrupt__" in step:
+        print("INTERRUPTED:")
+        interrupt = step["__interrupt__"][0]
+        for request in interrupt.value["action_requests"]:
+            print(request["description"])
+    else:
+        pass
